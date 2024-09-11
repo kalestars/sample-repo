d64d19014ff69a4fb98cfb30b37d3c98fbaa8768
Sara Ali <kalestars@gmail.com>
2024-09-11 21:41:04 +0300

    add file and folder

---
+++ script.sh
@@ -0,0 +1,6 @@
+pwd
+echo "Hello, I'm about to use script to create folder and files."
+mkdir evangadi-students
+touch test-1.txt text-2.txt index.html style.css
+echo "Hi, Just wrote this using from script..sh file">text-1.txt
+echo "thanks, I'm done"
